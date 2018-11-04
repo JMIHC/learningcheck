2c866ce4d54a57587c65dae7909d2070a57558c7
Benjamin Roedell <benjaminrobot@gmail.com>
2018-11-03 21:18:33 -0400

    Started trying to use react-bootstrap but having a hard time finding the @types for it

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as Nav from 'react-bootstrap/lib/Nav';
 import { BrowserRouter as BrowserRouter, Link, Route } from 'react-router-dom';
 import './App.css';
 import CheckAddition from './CheckAddition';
@@ -13,6 +14,25 @@ class App extends React.Component {
     return (
       <BrowserRouter>
       <div>
+        <Nav
+          activeKey="/home"
+          onSelect={selectedKey => alert(`selected ${selectedKey}`)}
+        >
+          <Nav.Item>
+            <Nav.Link href="/home">Active</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1">Link</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-2">Link</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="disabled" disabled>
+              Disabled
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
         <nav>
           <ul>
             <li>
